dcee0c2a269254bbcf7730f84a49a0d980478353
Amit Kandar <kandaramit2001@gmail.com>
2024-08-27 23:58:35 +0530

    fix: add custom api response

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import { asyncHandler } from '../utils/asyncHandler';
+import { APIResponse } from '../utils/APIResponse';
 
 export const register = asyncHandler(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const { username } = req.body;
-      res.status(200).json(username);
+      res
+        .status(201)
+        .json(new APIResponse(201, username, 'Register Successfully'));
     } catch (error) {
       next(error);
     }

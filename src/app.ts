a4026cef0e78cc239c679dbe30658fac571dc84e
Amit Kandar <kandaramit2001@gmail.com>
2024-08-30 00:27:08 +0530

    feat: Add helmet middleware in app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/errorHandler.middleware';
 import redisClient from './config/redis';
+import helmet from 'helmet';
 
 const app: Application = express();
 
@@ -12,6 +13,8 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(helmet());
 
 app.use(express.json({ limit: '16kb' }));
 
